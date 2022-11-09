91681bcb8adae14f8672f841150d752e307b4596
vovuhy <73991908+vovuhy@users.noreply.github.com>
2022-11-09 19:54:21 +0700

    update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # !/usr/bin/python
 # coding=utf-8
-from flask import Flask, config,jsonify,render_template
+from flask import Flask, config,jsonify,render_template,render_template_string
 
 from controllers.authController import *
 
@@ -11,8 +11,8 @@ app.register_blueprint(auth)
 
 @app.route('/')
 def index():
-    return "Hello, I'm Hy đẹp trai"
-    # return render_template('index2.html')
+    # return "Hello, I'm Hy đẹp trai"
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True,port=8113)
